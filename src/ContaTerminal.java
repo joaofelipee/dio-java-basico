0279463a0a1ad193924d6505a5d893248b76e9eb
João Felipe <brjoaof@gmail.com>
2023-11-16 15:32:21 -0300

    final commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,22 @@
 public class ContaTerminal {
+    int numero;
+    int agencia;
+    String nomeCliente;
+    double saldo;
+
+    public ContaTerminal(int numero, int agencia, String nome, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nome;
+        this.saldo = saldo;
+
+        mensagemBoasVindas();
+    }
+
+    private void mensagemBoasVindas() {
+        String mensagem = "Olá "+ this.nomeCliente+", obrigado por criar uma conta em nosso banco.\n";
+        mensagem = mensagem.concat("Sua agência é "+this.agencia+", conta "+ this.numero +" e seu saldo R$"+this.saldo+" já está disponível para saque.");
+
+        System.out.println(mensagem);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int numero, agencia;
+        String nome;
+        double saldo;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = scanner.nextInt();
+
+        //Limpa o buffer do scanner para evitar erro de leitura de tipos diferentes
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial da conta: ");
+        saldo = scanner.nextDouble();
+
+        ContaTerminal conta = new ContaTerminal(numero, agencia, nome, saldo);
     }
 }
